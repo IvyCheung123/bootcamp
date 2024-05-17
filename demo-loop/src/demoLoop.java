68e16d9eb720567350ab2ea4c4e2659c53f66407
ivy.cheung <ivycheung.eghc@gmail.com>
2024-05-18 00:43:00 +0800

    update

---
+++ demo-loop/src/demoLoop.java
@@ -0,0 +1,87 @@
+public class demoLoop {
+    public static void main(String[] args) {
+        int x = 3;
+        x *= 3; // 9
+        x *= 3; // 27
+        x *= 3; // 81
+
+        // for loop
+        // initialization (variable declaration & assignment) ; continous criteria ; incremental operation
+        // Step 1: int i = 0
+        // Step 2: check if i < 4 ? Yes
+        // Step 3: print "hello"
+        // Step 4: i++ -> 1
+        // ...
+        // i = 4 < 4 ? No
+        // No print
+        // exit the loop
+        for (int i = 0; i < 4; i++) { // 0,1,2,3
+            System.out.println("hello");
+        }
+        // hello
+        // hello
+        // hello
+        // hello
+
+        int y = 3;
+        for (int i = 0; i < 3; i++) {
+            y *= 3;
+        }
+        System.out.println("y = " + y); // y = 81
+
+        // 2
+        // 4
+        // 6
+        for (int i = 0; i < 7; i++) {
+            if (i % 2 == 0 && i != 0) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("\nBelow is sum = 55: ");
+        // 1 + 2 + 3 + 5 ... 10
+        int sum = 0;
+        // for (int i = 0; i < 55; i++) { sum++; }
+        for (int i = 0; i < 11; i++) {
+            sum += i;
+        }
+        System.out.println("sum = " + sum); // sum = 55
+
+        // 2 + 4 + 6 + 8 = 20
+        int sum2 = 0;
+        for (int i = 0; i < 9; i++) {
+            if (i % 2 == 0) {
+                sum2 += i;
+                System.out.println("sum2 = " + sum2);
+            }
+        }
+
+        // 8
+        // 6
+        // 4
+        // 2
+        for (int i = 9; i > 0; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        // break
+        for (int i = 0; i < 10; i++) {
+            if (i > 7) {
+                break; // exit loop
+            }
+            System.out.println(i); // main logic
+        } // 0 1 2 3 4 5 6 7
+
+        // continue
+        int total = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i > 3) {
+                continue; // when i = 4, skip "total += i", go to "i++"
+            }
+            total += i;
+        }
+        System.out.println("total = " + total);
+    }
+}
---
+++ demo-loop/src/demoNestedLoop.java
@@ -0,0 +1,69 @@
+public class demoNestedLoop {
+  public static void main(String[] args) {
+    for (int i = 0; i < 4; i++) { // 0,1,2,3
+      for (int j = 0; j < 2; j++) { // 0,1
+        // 0 -> 0,1
+        // 1 -> 0,1
+        // 2 -> 0,1
+        // 3 -> 0,1
+        System.out.println("hello i = " + i + ", j = " + j);
+      }
+    }
+    /*
+     * hello i = 0, j = 0
+     * hello i = 0, j = 1
+     * hello i = 1, j = 0
+     * hello i = 1, j = 1
+     * hello i = 2, j = 0
+     * hello i = 2, j = 1
+     * hello i = 3, j = 0
+     * hello i = 3, j = 1
+     */
+
+     System.out.println("\b");
+     /*
+     * hello i = 1, j = 0
+     * hello i = 3, j = 0
+     */
+
+     // Approach 2:
+    for (int i = 0; i < 4; i++) {
+        if (i % 2 == 0) {
+          continue;
+        }
+      for (int j = 0; j < 2; j++) {
+        if (j % 2 == 1) {
+          continue;
+        }
+        System.out.println("hello i = " + i + ", j = " + j);
+      }
+    }
+
+    System.out.println("\nBelow is exercise: ");
+    // Exercise
+    // 1 - 20
+    // 1 3 5 7 9 12 14 16 18 20
+    for (int i = 0; i < 21; i++) {
+      if (i > 0 && i < 10 && i % 2 == 1) {
+        System.out.println(i);
+      } else if (i > 10 && i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+
+    System.out.println("\b");
+
+    // Searching
+    String s = "Vincent Book Game Laptop";
+    // Find the index of the first character 'a'
+    // 14
+    int idx = 0;
+      for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == 'a') {
+          idx = i;
+          break; // exit loop, not if
+        }
+      }
+      System.out.println(idx);
+  }
+}
